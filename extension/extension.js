20a5e6fe09dd5682172441fe00cd3d5fc7f1f876
Prateek Kumar <prateek.kumar.original@gmail.com>
2026-08-04 15:14:03 +0530

    Restore image path pasting in terminals

--- extension/extension.js
+++ extension/extension.js
@@ -1382,7 +1382,7 @@ class ClipboardPopup {
     _activate(item, paste) {
         this.close();
         if (item.type === 'image') {
-            if (this._extension.isCodexCliWindow(this._previousWindow)) {
+            if (this._extension.isTerminalWindow(this._previousWindow)) {
                 this._extension.setClipboard(
                     this._extension.store.imagePath(item),
                     true
@@ -1494,11 +1494,6 @@ export default class ClipboardExtension extends Extension {
         ].filter(Boolean).join(' ').toLocaleLowerCase();
         return /terminal|kitty|alacritty|wezterm|foot|terminator|tilix|xterm|hyper|warp/
             .test(identity);
-    }
-
-    isCodexCliWindow(window) {
-        const title = (window?.get_title?.() ?? '').toLocaleLowerCase();
-        return this.isTerminalWindow(window) && /\bcodex(?:[-_\s]|$)/.test(title);
     }
 
     scheduleArchivePurge() {
